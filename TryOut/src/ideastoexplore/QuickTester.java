f7bc83260c7c7911938be9109ccb69152a92d000
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2020-08-29 02:54:56 +0530

    Update QuickTester.java

--- TryOut/src/ideastoexplore/QuickTester.java
+++ TryOut/src/ideastoexplore/QuickTester.java
@@ -3,11 +3,13 @@ package ideastoexplore;
 public class QuickTester {
 
 	public static void main(String[] args) {
-		String word1 = "Willow", word2 = "ow";
-        
-        System.out.println(word1 == "Willow");
-        System.out.println(word1 == ("Will"+"ow"));
-        System.out.println(word1 == ("Will"+word2));
+		Integer x = 128;
+		Integer y = 128;
+		System.out.println(x == y);
+		x = 128;
+		y = 128;
+		System.out.println("x = " + x + " y = " + y);
+		System.out.println(x == y);
 	}
 	
 }
